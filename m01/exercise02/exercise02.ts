88e77c0a497cdc1f9f6736fd47637e05fd2ad754
novanmasriandi11 <novanmasriandi@gmail.com>
2024-09-03 09:46:11 +0700

    Exercise 01 - Module 01 (All): Done

---
+++ m01/exercise02/exercise02.ts
@@ -0,0 +1,14 @@
+/*
+    QUESTION 1
+    Check whether the number is odd or even
+ */
+
+//Define variable and assign the value
+let _number: number = 25;
+
+//Process Check
+if (_number % 2 === 0) {
+    console.log(`${_number} is even`)
+} else {
+    console.log(`${_number} is odd`)
+} 
