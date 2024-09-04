4c95fc8055298b0d03d61800b26363571e3276ef
novanmasriandi11 <novanmasriandi@gmail.com>
2024-09-04 22:28:55 +0700

    Module 01 - Exercise 03 - Question 2: Done

--- m01/exercise03/exercise03.ts
+++ m01/exercise03/exercise03.ts
@@ -12,3 +12,17 @@ for (let x: number = 1; x <= _multiplyLength; x++) {
     let _output = `${_numberMultiplicator} x ${x}`
     console.log(_output)
 }
+
+/*
+    QUESTION 2
+    Check whether a string is a palindrome or not.
+ */
+
+//Define variable and assign the value 
+let _word: string = 'madam';
+let result: string = `${_word} -> `;
+
+//Process
+const reverseWord = _word.split('').reverse().join('');
+result = _word === reverseWord ? `${result}palindrome` : `${result} not palindrome`; 
+console.log(result);
