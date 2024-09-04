6f93ecbeb7c0ca05893da9df3a03291150d51c52
novanmasriandi11 <novanmasriandi@gmail.com>
2024-09-04 22:50:07 +0700

    Module 01 - Exercise 03 - Question 3: Done

--- m01/exercise03/exercise03.ts
+++ m01/exercise03/exercise03.ts
@@ -26,3 +26,17 @@ let result: string = `${_word} -> `;
 const reverseWord = _word.split('').reverse().join('');
 result = _word === reverseWord ? `${result}palindrome` : `${result} not palindrome`; 
 console.log(result);
+
+/*
+    QUESTION 3
+    Convert centimeter to kilometer
+ */
+
+//Define variable and assign the value
+let _distanceInCentiMeter: number = 100000;
+const conversionDivider: number = 100000;
+let _hasil = `${_distanceInCentiMeter} -> `;
+
+//Process
+_hasil += _distanceInCentiMeter / conversionDivider + 'km';
+console.log(_hasil)
