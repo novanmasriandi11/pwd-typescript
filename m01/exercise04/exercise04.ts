451668fba37c280d103d4066e233ca107fecc7ab
novanmasriandi11 <novanmasriandi@gmail.com>
2024-09-08 18:20:43 +0700

    Module 01 - Exercise 04 - Question 3: Done

--- m01/exercise04/exercise04.ts
+++ m01/exercise04/exercise04.ts
@@ -1,12 +1,13 @@
-/*
-    Question 1
-    Create a triangle pattern according to the height we provide like the following :
-    1
-    1  2
-    1  2  3
-    1  2  3  4
-    1  2  3  4  5
-*/
+/**
+ * Question 1
+ * Create a triangle pattern according to the height we provide like the following :
+ * 1
+ * 1  2
+ * 1  2  3
+ * 1  2  3  4
+ * 1  2  3  4  5
+ * @param height 
+ */
 
 //Solve
 function trianglePattern(height: number) {
@@ -22,13 +23,13 @@ function trianglePattern(height: number) {
 
 trianglePattern(5);
 
-/*
-    Question 2
-    Create a function that receiving array as input, 
-    and this function can find maximum value in array without using built in method in javascript.
-    Parameters : array
-    Output: number
-*/
+/**
+ * Question 2
+ * Create a function that receiving array as input,
+ * and this function can find maximum value in array
+ * without using built in method in javascript. 
+ * @param arrInput
+ */
 
 //Solve
 function findMaxValue(arrInput: number[]) {
@@ -42,3 +43,29 @@ function findMaxValue(arrInput: number[]) {
 }
 
 console.log(findMaxValue([10, 55, 79, 32]));
+
+/**
+ * Question 3
+ * Create a function that can create a triangle pattern according to the height we provide like the following :
+ * 01
+ * 02  03
+ * 04  05  06
+ * 07  08  09  10
+ * @param height
+ */
+
+//Solve
+function trianglePattern_2(height: number) {
+    let showPattern: string = '';
+    let currentNumber: number = 1;
+    for (let i = 1; i <= height; i++){
+        for (let j = 1; j <= i; j++){
+            showPattern += currentNumber < 10 ? `0${currentNumber} ` : currentNumber + ' ';
+            currentNumber++;
+        }
+        showPattern += '\n';
+    }
+    console.log(showPattern);
+}
+
+trianglePattern_2(4);
