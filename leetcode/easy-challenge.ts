30519a6b67e89495eb91bb27c95ed7a3a81d7f15
novanmasriandi11 <novanmasriandi@gmail.com>
2024-09-11 16:31:04 +0700

    Typescript challenge - Apply Transform Over Each Element in Array (leetCode)

--- leetcode/easy-challenge.ts
+++ leetcode/easy-challenge.ts
@@ -95,4 +95,26 @@ function filter(arr: number[], fn: Fn2): number[] {
 };
 console.log(filter([0, 10, 20, 30], function greaterThan10(n) { return n > 10; }));
 console.log(filter([1, 2, 3], function firstIndex(n, i) { return i === 0; }));
-console.log(filter([-2,-1,0,1,2], function plusOne(n) { return n + 1 }));
+console.log(filter([-2, -1, 0, 1, 2], function plusOne(n) { return n + 1 }));
+
+/**
+ * Apply Transform Over Each Element in Array
+ * 
+ * Given an integer array arr and a mapping function fn, 
+ * return a new array with a transformation applied to each element.
+ * The returned array should be created such that returnedArray[i] = fn(arr[i], i).
+ * Please solve it without the built-in Array.map method.
+ */
+
+//Solve
+function map(arr: number[], fn: (n: number, i: number) => number): number[] {
+    let result: number[] = [];
+    for (let x = 0; x < arr.length; x++){
+            result.push(fn(arr[x], x))
+    }
+
+    return result;
+};
+console.log(map([1, 2, 3], function plusone(n) { return n + 1; }));
+console.log(map([1, 2, 3], function plusI(n, i) { return n + i; }));
+console.log(map([10, 20, 30], function constant() { return 42; }));
