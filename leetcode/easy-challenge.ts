6b6f75353e0466643e98defc137532386dc8e7ea
novanmasriandi11 <novanmasriandi@gmail.com>
2024-09-12 13:23:26 +0700

    Typescript - Find the Width of Columns of a Grid (leetCode)

--- leetcode/easy-challenge.ts
+++ leetcode/easy-challenge.ts
@@ -118,3 +118,34 @@ function map(arr: number[], fn: (n: number, i: number) => number): number[] {
 console.log(map([1, 2, 3], function plusone(n) { return n + 1; }));
 console.log(map([1, 2, 3], function plusI(n, i) { return n + i; }));
 console.log(map([10, 20, 30], function constant() { return 42; }));
+
+/**
+ * Find the Width of Columns of a Grid
+ * 
+ * You are given a 0-indexed m x n integer matrix grid. 
+ * The width of a column is the maximum length of its integers.
+ * For example, if grid = [[-10], [3], [12]], 
+ * the width of the only column is 3 since -10 is of length 3.
+ * Return an integer array ans of size n where ans[i] is the width of the i column.
+ * The length of an integer x with len digits is equal to len if x is non-negative, 
+ * and len + 1 otherwise.
+ */
+
+//Solve
+function findColumnWidth(grid: number[][]): number[] {
+    let result: number[] = [];
+    for (let col = 0; col < grid[0].length; col++){
+        let maxLength = 0;
+        
+        for (let row = 0; row < grid.length; row++){
+            let numLength = grid[row][col].toString().length;
+            maxLength = Math.max(maxLength, numLength);
+        }
+
+        result.push(maxLength);
+    }
+    return result;
+};
+
+console.log(findColumnWidth([[1], [22], [333]]));
+// console.log(findColumnWidth([[-15,1,3],[15,7,12],[5,6,-2]]));
