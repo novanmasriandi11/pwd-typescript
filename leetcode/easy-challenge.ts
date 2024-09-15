18aa78e14f8e38f3c5d415753b32e1559d347d57
novanmasriandi11 <novanmasriandi@gmail.com>
2024-09-15 12:44:50 +0700

    Typescript - Generate Fibonacci Sequence (leetCode)

--- leetcode/easy-challenge.ts
+++ leetcode/easy-challenge.ts
@@ -295,7 +295,7 @@ function expect(val: any): ToBeOrNotToBe {
 /**
  * 11. Calculator with Method Chaining
  * 
- * Design a Calculator class. 
+ * Design a Calculator class.
  * The class should provide the mathematical operations of addition, subtraction, multiplication, division, and exponentiation. 
  * It should also allow consecutive operations to be performed using method chaining. 
  * The Calculator class constructor should accept a number which serves as the initial value of result.
@@ -355,3 +355,24 @@ const example2 = new Calculator(2).multiply(5).power(2).getResult();
 console.log(example2);
 
 // const example3 = new Calculator(20).divide(0).getResult();
+
+/**
+ * 12. Generate Fibonacci Sequence
+ * 
+ * Write a generator function that returns a generator object which yields the fibonacci sequence.
+ * The fibonacci sequence is defined by the relation Xn = Xn-1 + Xn-2.
+ * The first few numbers of the series are 0, 1, 1, 2, 3, 5, 8, 13.
+ */
+function* fibGenerator(): Generator<number, any, number> {
+    let x = 0, y = 1;
+    
+    while (true) {
+        yield x;
+        [x, y] = [y, x + y];
+    }
+};
+const gen = fibGenerator();
+console.log(gen.next().value);
+console.log(gen.next().value);
+console.log(gen.next().value);
+console.log(gen.next().value);
