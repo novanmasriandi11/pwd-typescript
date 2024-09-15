03b733ab24a1ad61d802aa4c36ac9f93c89d8a93
novanmasriandi11 <novanmasriandi@gmail.com>
2024-09-15 12:58:18 +0700

    Typescript LeetCode Easy Level - Array Reduce Transformation

--- leetcode/easy-challenge.ts
+++ leetcode/easy-challenge.ts
@@ -1,45 +1,4 @@
 /**
- * 1. Counter
- * Given an integer n, return a counter function. 
- * This counter function initially returns n 
- * and then returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
- */
-
-//Solve
-function createCounter(n: number): () => number {
-    return function inner() {
-        return n++;
-    }
-}
-const counter = createCounter(10);
-console.log(counter());
-console.log(counter());
-console.log(counter());
-
-/**
- * 2. Array Reduce Transformation
- * Given an integer array nums, a reducer function fn, and an initial value init, 
- * return the final result obtained by executing the fn function on each element of the array, 
- * sequentially, passing in the return value from the calculation on the preceding element.
- */
-
-//Solve
-type Fn = (accum: number, curr: number) => number
-
-function reduce(nums: number[], fn: Fn, init: number): number {
-    let result = init;
-    for (let x = 0; x < nums.length; x++) {
-        result = fn(result, nums[x]);
-    }
-
-    return result;
-};
-
-console.log(reduce([1, 2, 3, 4], function sum(accum, curr) { return accum + curr * curr; }, 100));
-console.log(reduce([1, 2, 3, 4], function sum(accum, curr) { return accum + curr; }, 0));
-console.log(reduce([], function sum(accum, curr) { return 0; }, 25));
-
-/**
  * 3. Function Composition
  * Given an array of functions [f1, f2, f3, ..., fn], 
  * return a new function fn that is the function composition of the array of functions.
