e1e18f943a06d3f584226005a4b204ea2d4900d8
novanmasriandi11 <novanmasriandi@gmail.com>
2024-09-15 13:08:27 +0700

    Typescript LeetCode Easy Level - To Be Or Not To Be

--- leetcode/easy-challenge.ts
+++ leetcode/easy-challenge.ts
@@ -1,20 +1,3 @@
- /**
- * 9. Create Hello World Function
- * 
- * Write a function createHelloWorld. It should return a new function that always returns "Hello World". 
- */
-
-function createHelloWorld() {
-    
-    return function(...args): string {
-        return "Hello World";        
-    };
-};
-
-const f = createHelloWorld();
-console.log(f());
-console.log(f({}, null, 42));
-
 /**
  * 10. To Be Or Not To Be
  * 
---
+++ leetcode/easy/to-be-or-not-to-be.ts
@@ -0,0 +1,37 @@
+/**
+ * 10. To Be Or Not To Be
+ * 
+ * Write a function expect that helps developers test their code. 
+ * It should take in any value val and return an object with the following two functions.
+    -   toBe(val) accepts another value and returns true if the two values === each other. 
+        If they are not equal, it should throw an error "Not Equal".
+    -   notToBe(val) accepts another value and returns true if the two values !== each other. 
+        If they are equal, it should throw an error "Equal".
+ */
+type ToBeOrNotToBe = {
+    toBe: (val: any) => boolean;
+    notToBe: (val: any) => boolean;
+};
+
+function expect(val: any): ToBeOrNotToBe {
+    return {
+        toBe: (otherValue: any) => {
+            if (val === otherValue) {
+                return true;
+            } else {
+                throw new Error("Not Equal");
+            }
+        },
+        notToBe: (otherValue: any) => {
+            if (val !== otherValue) {
+                return true;
+            } else {
+                throw new Error("Equal")
+            }
+        }
+    }    
+};
+console.log(expect(5).toBe(5));
+console.log(expect(5).toBe(null));
+console.log(expect(5).notToBe(5));
+console.log(expect(5).notToBe(null));
